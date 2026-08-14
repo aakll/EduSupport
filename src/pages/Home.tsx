a5a8772ba5c06ef739d3b8a6040a4b2fcd5456cd
Ali Kawar <alikawaar8@gmail.com>
2026-08-14 22:56:06 +0300

    update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -517,7 +517,7 @@ export default function Home() {
             {/* Bubble 2: Major? (Top Right of head) */}
             <div
               className="bubble-group group-2"
-              style={{ top: "20%", right: "30%" }}
+              style={{ top: "20%", right: "40%" }}
             >
               <div
                 className="bubble-dot-1"
@@ -538,7 +538,7 @@ export default function Home() {
             {/* Bubble 3: Scholarships? (Mid Right of head) */}
             <div
               className="bubble-group group-3"
-              style={{ top: "40%", right: "25%" }}
+              style={{ top: "40%", right: "35%" }}
             >
               <div
                 className="bubble-dot-1"
