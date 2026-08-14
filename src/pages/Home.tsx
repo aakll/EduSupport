4ca3041c2c0fe8c92776e79ff26d8ce5ddbd38a8
Ali Kawar <alikawaar8@gmail.com>
2026-08-14 15:46:20 +0300

    update pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,16 +56,7 @@ export default function Home() {
         </svg>
       ),
     },
-    {
-      name: "Undergraduate Student",
-      category: "university",
-      loggedInPath: "/university",
-      icon: (
-        <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.499 5.216 50.552 50.552 0 00-2.658.813m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15v4.5a2.25 2.25 0 002.25 2.25h6a2.25 2.25 0 002.25-2.25V15" />
-        </svg>
-      ),
-    },
+  
     {
       name: "Volunteer with Us",
       category: "volunteer",
